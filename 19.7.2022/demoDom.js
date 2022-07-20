631a461d09beb61fde1fdf962a8a713ff9f5d26a
Quý <quycao97@gmail.com>
2022-07-20 11:24:26 +0700

    demo

---
+++ 19.7.2022/demoDom.js
@@ -0,0 +1,14 @@
+// let a = document.getElementById("demo1");
+// let p = document.getElementById("demo2");
+// let inp = document.getElementById("demo3");
+// a.innerText = "QA";
+// p.innerText = "tam biet";
+// inp.value = "Quý Anh";
+// const p3 = document.createElement("p");
+// p3.innerText = "Phan trọng Quý";
+ var intro = "Tam Biet";
+ document.getElementById("intro").innerHTML = intro;
+ function myFunction(){
+    document.getElementById("demo").innerHTML = "Paragraph changed";
+
+ }
